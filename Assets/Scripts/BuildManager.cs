4506515cec260159d803331e9f852686e32f272f
Btopp <Niklas.Bachmann@ruhr-uni-bochum.de>
2017-09-17 17:12:01 +0200

    building system reworked

--- Assets/Scripts/BuildManager.cs
+++ Assets/Scripts/BuildManager.cs
@@ -45,36 +45,65 @@ public class BuildManager : MonoBehaviour {
 		mineSelected = false;
 	}
 
+//	public void BuildBuilding (int biomeID) {
+//		Vector3 pos = hexagonToBuildOn.TowerPosition.transform.position;
+//		if (mineSelected) {
+//			pos = hexagonToBuildOn.positionToPlace.transform.position;
+//			switch (biomeID) {
+//			case 1:
+//				buildingToBuild = mineMountain;
+//				break;
+//			case 2:
+//				buildingToBuild = mineOil;
+//				break;
+//			case 3:
+//				buildingToBuild = mineSand;
+//				break;
+//			default:
+//				break;
+//			}
+//		}
+//		Building b = buildingToBuild.GetComponent<Building> ();
+//		if (b.CanPay ()) {
+//			GameObject building = (GameObject) Instantiate (buildingToBuild, pos, Quaternion.identity);
+//			building.transform.parent = hexagonToBuildOn.transform;
+//			PlayerResources.DecreaseAmountOfResource (b.t1aCost, 1);
+//			PlayerResources.DecreaseAmountOfResource (b.t1bCost, 2);
+//			PlayerResources.DecreaseAmountOfResource (b.t1cCost, 3);
+//			if (mineSelected) {
+//				building.GetComponent<Mine> ().hexagonParent = hexagonToBuildOn;
+//				hexagonToBuildOn.buildingOnIt = buildingToBuild;
+//			}
+//			if (buildingToBuild == defence) {
+//				building.GetComponent<Defence> ().hexagonParent = hexagonToBuildOn;
+//				hexagonToBuildOn.buildingOnIt = buildingToBuild;
+//			}
+//		} else {
+//			Debug.Log ("Not enough resources");
+//		}
+//		ClearBuildingSelection ();
+//	}
+
+	//17.09.17 - n
 	public void BuildBuilding (int biomeID) {
-		Vector3 pos = hexagonToBuildOn.TowerPosition.transform.position;
 		if (mineSelected) {
-			pos = hexagonToBuildOn.positionToPlace.transform.position;
-			switch (biomeID) {
-			case 1:
-				buildingToBuild = mineMountain;
-				break;
-			case 2:
-				buildingToBuild = mineOil;
-				break;
-			case 3:
-				buildingToBuild = mineSand;
-				break;
-			default:
-				break;
-			}
+			buildingToBuild = hexagonToBuildOn.mineBuilding;
+		} else {
+			buildingToBuild = hexagonToBuildOn.defenceBuilding;
 		}
 		Building b = buildingToBuild.GetComponent<Building> ();
 		if (b.CanPay ()) {
-			GameObject building = (GameObject) Instantiate (buildingToBuild, pos, Quaternion.identity);
-			building.transform.parent = hexagonToBuildOn.transform;
 			PlayerResources.DecreaseAmountOfResource (b.t1aCost, 1);
 			PlayerResources.DecreaseAmountOfResource (b.t1bCost, 2);
 			PlayerResources.DecreaseAmountOfResource (b.t1cCost, 3);
 			if (mineSelected) {
+				hexagonToBuildOn.EnableMine ();
+				GameObject building = hexagonToBuildOn.mineBuilding;
 				building.GetComponent<Mine> ().hexagonParent = hexagonToBuildOn;
 				hexagonToBuildOn.buildingOnIt = buildingToBuild;
-			}
-			if (buildingToBuild == defence) {
+			} else {
+				hexagonToBuildOn.EnableDefence ();
+				GameObject building = hexagonToBuildOn.defenceBuilding;
 				building.GetComponent<Defence> ().hexagonParent = hexagonToBuildOn;
 				hexagonToBuildOn.buildingOnIt = buildingToBuild;
 			}
--- Assets/Scripts/Hexagon.cs
+++ Assets/Scripts/Hexagon.cs
@@ -10,16 +10,24 @@ public class Hexagon : MonoBehaviour {
 	public Biome biome;
 	[HideInInspector]
 	public GameObject buildingOnIt;
-	[HideInInspector]
-	public GameObject positionToPlace;
-	public GameObject TowerPosition;
+//	[HideInInspector]
+//	public GameObject positionToPlace;
+//	public GameObject TowerPosition;
 	[HideInInspector]
 	public BuildManager buildManager;
 
+	//17.09.17 - n
+	public GameObject defenceBuilding;
+	public GameObject mineBuilding;
+
 	void Start () {
-		positionToPlace = gameObject;
+//		positionToPlace = gameObject;
 		biome = GetComponent<Biome> ();
 		buildManager = GameObject.Find ("BuildManager").GetComponent<BuildManager> ();
+
+		//17.09.17 - n
+		defenceBuilding = transform.Find("Defence").gameObject;
+		mineBuilding = transform.Find("Mine").gameObject;
 	}
 		
 	void OnMouseDown () {
@@ -28,10 +36,19 @@ public class Hexagon : MonoBehaviour {
 		}
 		buildManager.hexagonToBuildOn = this;
 		if (buildingOnIt == null) {
-			if (!buildManager.mineSelected && buildManager.buildingToBuild == null) {
-				return;
-			}
+			//17.09.17 - Veraltet - n
+//			if (!buildManager.mineSelected && buildManager.buildingToBuild == null) {
+//				return;
+//			}
 			buildManager.BuildBuilding (biome.biomeType);
 		}
-	}		
+	}
+
+	public void EnableMine () {
+		mineBuilding.SetActive (true);
+	}
+
+	public void EnableDefence () {
+		defenceBuilding.SetActive (true);
+	}
 }
